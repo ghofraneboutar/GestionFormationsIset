070abd882f3229a159d17ab9098dbed4d241ad97
ghofraneboutar <157295811+ghofraneboutar@users.noreply.github.com>
2025-01-04 14:43:51 +0100

    Add files via upload

---
+++ admin-space.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { routes } from '../../app.routes';
+@Component({
+  selector: 'app-admin-space',
+  templateUrl: './admin-space.component.html',
+  styleUrl: './admin-space.component.css'
+})
+export class AdminSpaceComponent {
+title:string="admin-space"
+}
